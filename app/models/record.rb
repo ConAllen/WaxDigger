9ea96597b7c6b05bfdb9e735b69c166b3c2265e5
Con <allenconallen46@gmail.com>
2017-07-27 13:19:35 +0100

    final commit?

--- app/models/record.rb
+++ app/models/record.rb
@@ -3,8 +3,8 @@ class Record < ActiveRecord::Base
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 #the below validations validates the listing details
   validates :Title, :Label, :Format, :Country, :Released, :Genre, :Tracklist, :Condition, :Original_Price, :Selling_Price, Presence: true
-# validates price of listings ** need to fix this
-  validates :Original_Price, numericality: { greater_than: 0, less_than: 18}
+# validates price of record listings 
+  validates :Original_Price, numericality: { greater_than: 0, less_than: 25}
   validates :Selling_Price, numericality: { greater_than: 0, less_than: 50}
 #validates image are present
   validates :image, attachment_presence: true
